1a9d5513268149bc1995fc10d9a3e91521d4df0f
ic1149 <126190900+ic1149@users.noreply.github.com>
2024-02-05 17:53:33 +0000

    Update datecal.js
    counting animation

--- datecal.js
+++ datecal.js
@@ -1,5 +1,37 @@
-const date1 = new Date('1/9/2024');
-const date2 = new Date();
-const diffTime = Math.abs(date2 - date1);
-const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-document.getElementById("daycount").innerHTML = diffDays + " days";
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function unitcounting() {
+  const target = 3
+  const duration = 1
+  const interval = (duration / target) * 1000
+  for (let i = 0; i <= target; i++) {
+    document.getElementById("unitcount").innerHTML = String(i) + "/90";
+    await sleep(interval);
+
+  }
+
+}
+
+async function daycounting() {
+  const date1 = new Date('1/9/2024');
+  const date2 = new Date();
+  const diffTime = Math.abs(date2 - date1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+  const duration = 1
+  const interval = (duration / target) * 1000
+  for (let i = 0; i <= diffTime; i++) {
+    document.getElementById("daycount").innerHTML = String(i) + " days";
+    await sleep(interval);
+
+  }
+
+}
+
+unitcounting()
+daycounting()
+
+
+
+
