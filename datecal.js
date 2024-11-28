0c6a06162f6f114c44914f15f6e5dba5ed9e1ab7
ic1149 <126190900+ic1149@users.noreply.github.com>
2024-11-28 08:51:33 +0000

    Update datecal.js
    2 more units, 5 -> 7

--- datecal.js
+++ datecal.js
@@ -3,7 +3,7 @@ function sleep(ms) {
 }
 
 async function unitcounting() {
-  const target = 5
+  const target = 7
   const duration = 1
   const interval = (duration / target) * 1000
   for (let i = 0; i <= target; i++) {
